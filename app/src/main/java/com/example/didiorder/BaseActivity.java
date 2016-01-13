8e48e4de8593f456807e768a1ac60ca42d59a19f
fallllllll <348476129@qq.com>
2016-01-13 14:46:58 +0800

    例行提交

---
+++ app/src/main/java/com/example/didiorder/BaseActivity.java
@@ -0,0 +1,61 @@
+package com.example.didiorder;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+public class BaseActivity extends AppCompatActivity {
+private SystemBarTintManager tintManager;
+    private View contentView;
+    private LinearLayout ly_content;
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams. FLAG_TRANSLUCENT_NAVIGATION);
+        setContentView(R.layout.activity_base);
+        ly_content = (LinearLayout) findViewById(R.id.ly_content);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+         tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setNavigationBarTintEnabled(true);
+    }
+    /***
+     * 设置内容区域
+     *
+     * @param resId
+     *            资源文件ID
+     */
+    public void setContentLayout(int resId) {
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        contentView = inflater.inflate(resId, null);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.FILL_PARENT);
+        contentView.setLayoutParams(layoutParams);
+        contentView.setBackgroundDrawable(null);
+        if (null != ly_content) {
+            ly_content.addView(contentView);
+        }
+
+    }
+    public void setStarusBarBackground(int id){
+        tintManager.setStatusBarTintResource(id);
+    }
+    public void setNavigationBarBackground(int id){
+        tintManager.setNavigationBarTintResource(id);
+    }
+    public Toolbar getToolbar(){
+        return toolbar;
+    }
+}
--- app/src/main/java/com/example/didiorder/activity/LoginActivity.java
+++ app/src/main/java/com/example/didiorder/activity/LoginActivity.java
@@ -8,27 +8,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
 
+import com.example.didiorder.BaseActivity;
 import com.example.didiorder.R;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
     private  Toolbar toolbar ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getWindow().addFlags(WindowManager.LayoutParams. FLAG_TRANSLUCENT_NAVIGATION);
-        setContentView(R.layout.activity_login);
+        setContentLayout(R.layout.activity_login);
         initView();
+        initClick();
+    }
+    private void initClick(){
+        toolbar.setNavigationOnClickListener(v -> finish());
     }
 private void initView(){
-    SystemBarTintManager tintManager = new SystemBarTintManager(this);
-    tintManager.setStatusBarTintEnabled(true);
-    tintManager.setNavigationBarTintEnabled(true);
-    tintManager.setNavigationBarTintResource(R.mipmap.navigation);
-    tintManager.setStatusBarTintResource(R.mipmap.status);
-    toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setStarusBarBackground(R.mipmap.status);
+    setNavigationBarBackground(R.mipmap.navigation);
+    toolbar = getToolbar();
     toolbar.setTitle("登录");
     setSupportActionBar(toolbar);
+    toolbar.setNavigationIcon(R.mipmap.ic_arrow_back_white_24dp);
 }
 }
